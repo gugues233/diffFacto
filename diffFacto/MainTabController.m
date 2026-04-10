953f88e481f853227267d68d78f9a47cb0ba94dd
gugues233 <gugues233@outlook.com>
2026-04-10 18:53:15 +0800

    tabBar带颜色及底部线（非理想下划线、滑动切换）

--- diffFacto/MainTabController.m
+++ diffFacto/MainTabController.m
@@ -6,11 +6,12 @@
 //
 
 #import "MainTabController.h"
-
 #import "CarouselViewController.h"
 #import "HomeViewController.h"
 
-@interface MainTabController ()
+#define TextSize 20
+
+@interface MainTabController () <UITabBarControllerDelegate>
 
 @end
 
@@ -18,19 +19,70 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-     // 1. 创建所有页面
-    CarouselViewController *carousel = [[CarouselViewController alloc] init];
-    HomeViewController *home = [[HomeViewController alloc] init];
-     
-     // 2. 设置tab内容
-    carousel.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"创作" image:nil tag:0];
-    home.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"设计室" image:nil tag:1];
-     
-     // 3. 把自己的子页面 赋值给自己
-     self.viewControllers = @[carousel, home];
+    [self setUpViewControllers];
+    [self setUpTabItemTextStyle];
+    [self setupUnderlineObserver];
+}
 
+// 创建页面并加入tabbar
+- (void)setUpViewControllers {
+    // 1. 创建所有页面
+   CarouselViewController *carousel = [[CarouselViewController alloc] init];
+   HomeViewController *home = [[HomeViewController alloc] init];
+    
+    // 2. 设置tab内容
+   carousel.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"创作" image:nil tag:0];
+   home.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"设计室" image:nil tag:1];
+    
+    // 3. 把自己的子页面 赋值给自己
+    self.viewControllers = @[carousel, home];
+}
+
+// 设置tabbar中文字样式
+- (void)setUpTabItemTextStyle {
+    NSDictionary *normal = @{
+        NSFontAttributeName: [UIFont systemFontOfSize:TextSize],
+        NSForegroundColorAttributeName: [UIColor blackColor]
+    };
+    NSDictionary *selected = @{
+        NSFontAttributeName: [UIFont systemFontOfSize:TextSize],
+        NSForegroundColorAttributeName: [UIColor blueColor]
+    };
+    
+    [UITabBarItem.appearance setTitleTextAttributes:normal forState:UIControlStateNormal];
+    [UITabBarItem.appearance setTitleTextAttributes:selected forState:UIControlStateSelected];
+}
+
+- (void) setupUnderlineObserver {
+    self.delegate = self;
+}
+
+- (void)tabBarController:(UITabBarController *)tabBarController didSelectViewController:(UIViewController *)viewController {
+    [self updateUnderline];
 }
 
 
+// 更新下划线
+- (void)updateUnderline {
+    for (UIView *view in self.tabBar.subviews) {
+        if (view.tag == 9999) [view removeFromSuperview];
+    }
+    
+    NSInteger count = self.viewControllers.count;
+    CGFloat w = self.tabBar.bounds.size.width / count;
+    CGFloat x = self.selectedIndex * w;
+    
+    UIView *line = [[UIView alloc] initWithFrame:CGRectMake(x, self.tabBar.bounds.size.height - 2, w, 2)];
+    line.backgroundColor = [UIColor blueColor];
+    line.tag = 9999;
+    [self.tabBar addSubview:line];
+}
+
+// 页面刷新时
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [self updateUnderline];
+}
+
 @end
+
